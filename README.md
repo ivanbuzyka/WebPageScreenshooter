1c74662b86a5f27c22c1b5dd3d8f8d0c2ac50289
ivanbuzyka <ivanbuzyka@gmail.com>
2021-08-04 17:48:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# WebPageScrenshooter
+Simple console app that can be used to produce screenshots of the web page.
+The app can be scheduled to run in any scheduler.
+
+## Getting started
+1. Restore Nuget packages, build project
+2. Make sure Node JS is installed on your machine
+3. Run ```npx playwright install``` in the folder with executable produced by build
+4. Run exe wth parameters (or schedule it to run). Example:
+    ```WebPageScreenshooter.exe https://www.forex.com/ie/trading-academy/courses/how-to-trade/buying-and-selling/ 60000```
+    **Note:** second parameter is a timeout for page request in milliseconds
---
+++ WebPageScreenshooter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace WebPageScreenshooter
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string hostName = args[0];
+            float timeout = 30000;
+            float.TryParse(args[1], out timeout);
+
+            Console.WriteLine($"Page request timeout: {timeout}");
+            Console.WriteLine($"Going to take a screenshot from {hostName}");
+
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = true });
+            var context = await browser.NewContextAsync();
+            context.SetDefaultTimeout(timeout);
+            var page = await context.NewPageAsync();
+            await page.GotoAsync(hostName);
+            var screenshotName = $"{ DateTime.UtcNow.ToString("yyyy-MM-dd_HH.mm.ss") }.png";
+            var filePath = Path.GetFullPath($"./screenshots/{screenshotName}");
+            await page.ScreenshotAsync(new() { FullPage = true, Path = filePath });
+
+            await context.CloseAsync();
+        }
+    }
+}
